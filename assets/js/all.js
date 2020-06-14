6d47ec2cf073fc9411e43518c0b32322590e1e17
alivealife <alivealife@hotmail.com>
2020-06-14 19:35:01 +0800

    Update 2020-06-14T11:34:41.991Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,13 +1,16 @@
+"use strict";
+
 $(document).ready(function () {
   $('.ham-icon').click(function (e) {
     e.preventDefault();
-    $('.nav-menu').toggleClass('active')
-  })
+    $('.nav-menu').toggleClass('active');
+  });
   $('.answer').hide();
   $('.question').click(function (e) {
     $(this).toggleClass('show');
     $(this).siblings('.answer').slideToggle();
     $(this).parent().siblings().find('.answer').slideUp();
     $(this).parent().siblings().find('.show').removeClass('show');
-  })
+  });
 });
+//# sourceMappingURL=all.js.map
